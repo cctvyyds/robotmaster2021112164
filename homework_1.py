f4adc7180e07035245323fa2e2460de1f7bfa079
沙雕网友 <92567564+cctvyyds@users.noreply.github.com>
2022-09-19 13:10:10 +0800

    Add files via upload

--- homework_1.py
+++ homework_1.py
@@ -1,33 +1,41 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[1]:
 
 
 import numpy as np
 path="data_20220918210128.csv"
 
 
-# In[3]:
-
-
-X = np.loadtxt(path, delimiter=",", dtype="float",usecols = (0,1,2))
-
-
 # In[4]:
 
 
-y=np.loadtxt(path, delimiter=",", dtype="float",usecols = (3))
+X = np.loadtxt(path, delimiter=",", dtype="float",usecols = (0,1,2))
+x0=np.ones(X.shape[0])
+X=np.insert(X,3,x0,axis=1)
 
 
 # In[6]:
 
 
-W = np.linalg.inv(X.T@X)@X.T@y
+y=np.loadtxt(path, delimiter=",", dtype="float",usecols = (3))
 
 
 # In[7]:
 
 
+W = np.linalg.inv(X.T@X)@X.T@y
+
+
+# In[8]:
+
+
 print(W)
+
+
+# In[ ]:
+
+
+
 
